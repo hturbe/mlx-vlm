8c4a78b1adb971c39bdc87079ce18bb5e1c29449
hturbe <hugues.turbe@gmail.com>
2025-11-11 15:25:15 +0100

    minor

--- mlx_vlm/__init__.py
+++ mlx_vlm/__init__.py
@@ -2,6 +2,7 @@ import os
 
 from .convert import convert
 from .generate import GenerationResult, generate, stream_generate
+from .batch_generate import batch_generate
 from .prompt_utils import apply_chat_template, get_message_json
 from .utils import load, prepare_inputs, process_image
 from .version import __version__
